2917931a0986232d400c351a77ae8cf09275cc51
Tushar Patil <tusharpatil17012001@gmail.com>
2025-08-09 22:46:58 +0530

    fixed

--- server/index.js
+++ server/index.js
@@ -148,13 +148,5 @@ app.post("/compile", async (req, res) => {
     }
 });
 
-// Remove these lines as the frontend is now served separately.
-/*
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-*/
-
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
